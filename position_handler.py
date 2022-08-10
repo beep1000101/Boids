455df2cb8022e0d24b4fe3ec6f013fd079fb6063
Mateusz Poniatowski <matiponiatowski@o2.pl>
2022-08-10 22:25:14 +0200

    position handler class update method update

--- position_handler.py
+++ position_handler.py
@@ -7,17 +7,15 @@ class PositionHandler:
         self.__flock_positions_x = flock_positions_x
         self.__flock_positions_y = flock_positions_y
 
-    def update_x(self, flock_positions_x: array) -> None:
-        self.__flock_positions_x = flock_positions_x
-
-    def update_y(self, flock_positions_y: array) -> None:
-        self.__flock_positions_y = flock_positions_y
-
     def __create_mirror_images(self) -> None:
         # finish algorithm creating mirror images to properly
         # create a neighborhood on torus
         self.__mirror_images_x = None
         self.__mirror_images_y = None
+
+    def update_positions(self, flock_positions_x: array, flock_positions_y: array) -> None:
+        self.__flock_positions_x = flock_positions_x
+        self.__flock_positions_y = flock_positions_y
 
     def get_mirror_images_x(self) -> array:
         return self.__mirror_images_x
