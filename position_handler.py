e270465cb1a7ce1663a6b2fb2a7efc5389651d83
Mateusz Poniatowski <matiponiatowski@o2.pl>
2022-08-10 22:11:14 +0200

    position handler cleaning

--- position_handler.py
+++ position_handler.py
@@ -1,17 +1,16 @@
-import numpy as np
-
 from boids import Flock
+from numpy import array
 
 
 class PositionHandler:
-    def __init__(self, flock_positions_x: np.array, flock_positions_y: np.array) -> None:
+    def __init__(self, flock_positions_x: array, flock_positions_y: array) -> None:
         self.__flock_positions_x = flock_positions_x
         self.__flock_positions_y = flock_positions_y
 
-    def update_x(self, flock_positions_x: np.array) -> None:
+    def update_x(self, flock_positions_x: array) -> None:
         self.__flock_positions_x = flock_positions_x
 
-    def update_y(self, flock_positions_y: np.array) -> None:
+    def update_y(self, flock_positions_y: array) -> None:
         self.__flock_positions_y = flock_positions_y
 
     def __create_mirror_images(self) -> None:
@@ -20,8 +19,8 @@ class PositionHandler:
         self.__mirror_images_x = None
         self.__mirror_images_y = None
 
-    def get_mirror_images_x(self) -> np.array:
+    def get_mirror_images_x(self) -> array:
         return self.__mirror_images_x
 
-    def get_mirror_images_y(self) -> np.array:
+    def get_mirror_images_y(self) -> array:
         return self.__mirror_images_y
