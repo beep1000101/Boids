bfd158f04e76420dba5569829ad1f77e128567cf
Mateusz Poniatowski <matiponiatowski@o2.pl>
2022-08-10 22:17:17 +0200

    main script cleaning

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from config_parser import ConfigParser
 
 
 def main():
-    cofig_parser = ConfigParser()
+    cp = ConfigParser()
 
     def animate(i, flock_object: Flock, quiver_object, dt):
         flock_object.update_flock_state(dt)
@@ -16,27 +16,27 @@ def main():
                                   flock.get_velocity_versors_x(),
                                   flock.get_velocity_versors_y(),
                                   flock.get_phi_angles(),
-                                  pivot=cofig_parser.get_quiver_pivot())
+                                  pivot=cp.get_quiver_pivot())
 
-        plt.xlim(*cofig_parser.get_matplotlib_xlim_params())
-        plt.ylim(*cofig_parser.get_matplotlib_ylim_params())
+        plt.xlim(*cp.get_matplotlib_xlim_params())
+        plt.ylim(*cp.get_matplotlib_ylim_params())
         return quiver_object
 
-    flock = Flock(**cofig_parser.get_flock_params())
+    flock = Flock(**cp.get_flock_params())
 
-    fig, ax = plt.subplots(figsize=cofig_parser.get_figsize())
+    fig, ax = plt.subplots(figsize=cp.get_figsize())
 
     Q = ax.quiver(flock.get_positions_x(),
                   flock.get_positions_y(),
                   flock.get_velocity_versors_x(),
                   flock.get_velocity_versors_y(),
                   flock.get_phi_angles(),
-                  pivot=cofig_parser.get_quiver_pivot())
+                  pivot=cp.get_quiver_pivot())
 
     anim = animation.FuncAnimation(fig, animate, fargs=(flock, Q, 0.01), interval=50, blit=False)
 
-    plt.xlim(*cofig_parser.get_matplotlib_xlim_params())
-    plt.ylim(*cofig_parser.get_matplotlib_ylim_params())
+    plt.xlim(*cp.get_matplotlib_xlim_params())
+    plt.ylim(*cp.get_matplotlib_ylim_params())
     plt.show()
 
 
