a92a374a07fb4a969b0689c8429f6a4a05333531
Mateusz Poniatowski <matiponiatowski@o2.pl>
2022-08-10 22:24:29 +0200

    brain class dunder fix

--- brain.py
+++ brain.py
@@ -6,7 +6,7 @@ class Brain:
     def __init__(self) -> None:
         pass
 
-    def __find_nearest_neigbors(self) -> None:
+    def find_nearest_neigbors(self) -> None:
         pass
 
     def get_nearest_neighbors(self) -> array:
