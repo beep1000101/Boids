2847d0de313548699db5a336b75ae0df6b44539c
Mateusz Poniatowski <matiponiatowski@o2.pl>
2022-08-10 22:10:02 +0200

    initial brain class

---
+++ brain.py
@@ -0,0 +1,13 @@
+from sklearn.neighbors import BallTree
+from numpy import array
+
+
+class Brain:
+    def __init__(self) -> None:
+        pass
+
+    def __find_nearest_neigbors(self) -> None:
+        pass
+
+    def get_nearest_neighbors(self) -> array:
+        pass
