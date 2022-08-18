6b6fb5815a9372bf7b109e6cd16dd4e419681f3b
Mateusz Poniatowski <matiponiatowski@o2.pl>
2022-08-18 20:15:41 +0200

    config parser revamp

--- config_parser.py
+++ config_parser.py
@@ -3,50 +3,60 @@ import configparser
 from os import path
 
 
-class ConfigParser:
+class ConfigParserBaseClass:
     def __init__(self) -> None:
         self.config = configparser.ConfigParser()
-        __path = path.join(path.realpath(path.dirname(__name__)), "config.ini")
-        self.config.read(__path)
+        _path = path.join(path.realpath(path.dirname(__name__)), "config.ini")
+        self.config.read(_path)
 
-    """FLOCK PART"""
+    """FLOCK PARAMS"""
 
-    def get_flock_params(self) -> dict:
-        return {key: int(value) for key, value in self.config["flock_params"].items()}
+    def _get_number_of_boids(self) -> int:
+        return int(self.config["flock_params"]["number_of_boids"])
 
-    """FIGURE PART"""
+    def _get_boid_max_velocity(self) -> int:
+        return int(self.config["flock_params"]["boid_velocity_max"])
 
-    def get_matplotlib_xlim_params(self) -> tuple:
-        return tuple([int(value) for key, value in self.config["matplotlib_params"].items() if key.startswith("xlim")])
+    """MATPLOTLIB PARAMS"""
 
-    def get_matplotlib_ylim_params(self) -> tuple:
-        return tuple([int(value) for key, value in self.config["matplotlib_params"].items() if key.startswith("ylim")])
+    def _get_xlim_min(self) -> int:
+        return int(self.config["matplotlib_params"]["xlim_min"])
 
-    def get_figsize(self) -> tuple:
-        return tuple(int(value) for value in self.config["matplotlib_params"]["figsize"].split(","))
+    def _get_xlim_max(self) -> int:
+        return int(self.config["matplotlib_params"]["xlim_max"])
 
-    def get_quiver_pivot(self) -> str:
+    def _get_ylim_min(self) -> int:
+        return int(self.config["matplotlib_params"]["ylim_min"])
+
+    def _get_ylim_max(self) -> int:
+        return int(self.config["matplotlib_params"]["ylim_max"])
+
+    def _get_figsize(self) -> int:
+        return int(self.config["matplotlib_params"]["figsize"])
+
+    """QUIVER PARAMS"""
+
+    def _get_quiver_pivot(self) -> str:
         return self.config["quiver_params"]["quiver_pivot"]
 
-    """ANIMATION PART"""
+    """ANIMATION PARAMS"""
 
-    def get_animation_dt(self) -> float:
+    def _get_dt(self) -> float:
         return float(self.config["animation_params"]["dt"])
 
-    def get_animation_kwargs(self) -> dict:
-        _content = self.config["animation_params"]["kwargs"].split(",")
-        return {"interval": int(_content[0]), "blit": bool(int(_content[1]))}
+    def _get_interval(self) -> int:
+        return int(self.config["animation_params"]["interval"])
 
-    """UNIVERSE SIZE PART"""
+    def _get_blit(self) -> bool:
+        return bool(int(self.config["animation_params"]["blit"]))
 
-    def get_left_bound(self) -> int:
+    """UNIVERSE SIZE"""
+
+    def _get_left_bound(self) -> int:
         return int(self.config["universe_size"]["left_bound"])
 
-    def get_right_bound(self) -> int:
-        return self.get_left_bound() + int(self.config["universe_size"]["distance"])
-
-    def get_lower_bound(self) -> int:
+    def _get_lower_bound(self) -> int:
         return int(self.config["universe_size"]["lower_bound"])
 
-    def get_upper_bound(self) -> int:
-        return self.get_lower_bound() + int(self.config["universe_size"]["distance"])
+    def _get_distance(self) -> int:
+        return int(self.config["universe_size"]["distance"])
