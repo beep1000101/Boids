1d14b38fb60366364e4013c37166720d22cab6ec
Mateusz Poniatowski <matiponiatowski@o2.pl>
2022-11-04 00:52:25 +0100

    2 rules implemented

--- boids.py
+++ boids.py
@@ -6,6 +6,8 @@ from numpy import stack
 from numpy import array
 from numpy import sin
 from numpy import cos
+from numpy.random import rand
+from brain import Brain
 
 
 class Flock():
@@ -17,24 +19,20 @@ class Flock():
                  lower_bound: float = 0,
                  distance: float = 10) -> None:
 
-        number_of_boids: int = 1 if number_of_boids <= 0 else number_of_boids
         seed: float = time() + uniform(-1000, 1000)
         SEED(str(seed).encode("utf-8"))
 
+        self.number_of_boids: int = 1 if number_of_boids <= 0 else number_of_boids
         self.velocity: float = velocity_max
         self.left_bound: float = left_bound
         self.lower_bound: float = lower_bound
         self.velocity_max: float = velocity_max
         self.distance: float = distance
 
-        self.positions: array[array[float]] = array([
-            array([
-                uniform(left_bound, left_bound + distance),
-                uniform(lower_bound, lower_bound + distance)
-            ]) for _ in range(number_of_boids)
-        ])
-        self.angles = array([uniform(0, 2 * pi) for _ in range(number_of_boids)])
-        self.velocities = array([self.velocity for _ in range(number_of_boids)])
+        self.positions: array[
+            array[float]] = rand(number_of_boids, 2) * (distance - left_bound) + left_bound
+        self.angles: array[float] = array([uniform(0, 2 * pi) for _ in range(number_of_boids)])
+        self.velocities: array[float] = array([self.velocity for _ in range(number_of_boids)])
 
     def get_positions_x(self) -> array:
         return self.positions[:, 0]
@@ -58,8 +56,18 @@ class Flock():
         return cos(self.angles)
 
     def update_flock_state(self, dt: float) -> None:
+        # .5, .3 ciekawe rzeczy robia
+        visibility_radius = 4
+        mass_center_weight = .5
+        avg_angle_weight = .03
+        mass_center_vector, avg_angle_vector = Brain.get_nearest_neighbors(
+            self.positions, visibility_radius, self.angles, self.number_of_boids)
+        # wywalic angles
+        neighbour_mass = mass_center_weight * (mass_center_vector - self.angles)
+        neighbor_angles = avg_angle_weight * (avg_angle_vector - self.angles)
         self.angles = self.angles + array(
-            [12 * uniform(-pi / 180, pi / 180) for _ in range(len(self.angles))])
+            [12 * uniform(-pi / 180, pi / 180)
+             for _ in range(len(self.angles))]) + neighbour_mass + neighbor_angles
         self.positions = self.positions + dt * stack(
             (self.get_velocities_x(), self.get_velocities_y()), axis=1)
         self.positions[:, 0] = (self.positions[:, 0] + self.left_bound
--- brain.py
+++ brain.py
@@ -1,13 +1,89 @@
 from sklearn.neighbors import BallTree
 from numpy import array
+from numpy import concatenate
+from numpy import nan
+from numpy import isnan
+from numpy import dot
+from numpy.linalg import norm
+from numpy import arccos
+# from numpy import angle as vector_angle
+
+center_of_the_universe = [component / 2 for component in (10, 10)]
 
 
 class Brain:
-    def __init__(self) -> None:
+
+    @staticmethod
+    def find_nearest_neigbors() -> None:
         pass
 
-    def find_nearest_neigbors(self) -> None:
-        pass
+    @staticmethod
+    def between(number):
+        if number > 1:
+            return 1
+        elif number < -1:
+            return -1
+        return number
 
-    def get_nearest_neighbors(self) -> array:
-        pass
+    @classmethod
+    def get_nearest_neighbors(cls, boid_positions: array, visibility_radius: float, flock_angles,
+                              boid_number) -> array:
+        mirror_images = cls._get_mirror_images(boid_positions)
+
+        ball_tree = BallTree(mirror_images, leaf_size=2)
+
+        distances, boid_indices = ball_tree.query(boid_positions, k=5)
+
+        distances = distances[:, 1:]
+        indices = boid_indices[:, 1:]
+        indices = indices % boid_number
+        # Mask distances that exceed visibility radius
+        distances[~(distances < visibility_radius)] = nan
+        # Use mask on indicies
+        indices = indices * distances / distances
+        indices = array([row[~isnan(row)].astype(int) for row in indices], dtype=object)
+
+        mass_center_vector = []
+        avg_angle_vector = []
+        # wziac kat miedzy boidem a sasiadem
+        for boid_index, row in enumerate(indices):
+            my_index = boid_indices[boid_index, 0] % boid_number
+            my_position = boid_positions[my_index]
+            # mass_center = [
+            #     arccos(
+            #         cls.between(
+            #             dot(boid_positions[index], my_position) /
+            #             (norm(boid_positions[index]) * norm(my_position)))) for index in row
+            # ] if row.size > 0 else 0
+            mass_center = [boid_positions[index] for index in row] if row.size > 0 else None
+            mass_center = sum(mass_center) / len(mass_center) if mass_center else 0
+            mass_center = arccos(
+                cls.between(
+                    dot(mass_center, my_position) /
+                    (norm(mass_center) * norm(my_position)))) if norm(mass_center) else 0
+
+            avg_angle = [flock_angles[index] for index in row]
+            avg_angle = sum(avg_angle) / len(avg_angle) if avg_angle else 0
+
+            mass_center_vector.append(mass_center)
+            avg_angle_vector.append(avg_angle)
+
+        return array(mass_center_vector), array(avg_angle)
+
+    @staticmethod
+    def _get_mirror_images(boid_positions: array) -> array:
+        left_image = boid_positions[boid_positions[:, 0] > center_of_the_universe[0]]
+        left_image[:, 0] = left_image[:, 0] - 2 * center_of_the_universe[0]
+
+        right_image = boid_positions[boid_positions[:, 0] < center_of_the_universe[0]]
+        right_image[:, 0] = right_image[:, 0] + 2 * center_of_the_universe[0]
+
+        top_image = boid_positions[boid_positions[:, 1] < center_of_the_universe[1]]
+        top_image[:, 1] = top_image[:, 1] + 2 * center_of_the_universe[1]
+
+        bottom_image = boid_positions[boid_positions[:, 1] > center_of_the_universe[1]]
+        bottom_image[:, 1] = bottom_image[:, 1] - 2 * center_of_the_universe[1]
+
+        mirror_images = concatenate((left_image, right_image, top_image, bottom_image))
+
+        return mirror_images
